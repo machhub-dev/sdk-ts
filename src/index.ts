35352d387164e248d519f13c2e263a142a1a424a
Reuben Lee Suppiah <reuben.leesuppiah@intellogic.com.my>
2025-05-05 17:23:48 +0800

    feat(MCH-228): Export Types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export { SDK } from './sdk-ts';
 export { getAppConfig } from './utils/appConfig';
+
+// Export all types
+export * from './types/auth.models';
+export * from './types/response.models';
+export * from './types/recordID.models';
+export * from './types/tag.models';
